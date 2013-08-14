0c5c43b2a50ef7c9c7ce9eece89472d233c876d1
Christopher Kong <cgkong@gmail.com>
2013-08-14 14:50:51 -0400

    evaluate v3
    30/07/2013 9:18PM

--- evaluate.m
+++ evaluate.m
@@ -1,110 +1,101 @@
-clear all;
-clc;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-outputFile = ('output.csv');
-gtFile = ('groundTruth_perfectdata.csv');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ X ] = evaluate(detectionScore)
 
-outputDirectory = ('data/extendedDataset/output/');
-gtDirectory = ('data/extendedDataset/output/groundTruth/');
+	clc;
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	outputFile = ('output_allData.csv');
+	gtFile = ('groundTruth.csv');
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-fileID = fopen(strcat(outputDirectory, outputFile));
-C = textscan(fileID, '%s %d %d %d %d','delimiter', ',', 'EmptyValue', -Inf);
-fclose(fileID);
-
-detectedImgs = char(C{1});
-detectedRegionsX = int32(C{2});
-detectedRegionsY = int32(C{3});
-detectedRegionsW = int32(C{4});
-detectedRegionsH = int32(C{5});
-
-fileID = fopen(strcat(gtDirectory, gtFile));
-C = textscan(fileID, '%s %d %d %d %d','delimiter', ',', 'EmptyValue', -Inf);
-fclose(fileID);
-
-groundTruthPosImgs = char(C{1});
-groundTruthPosRegionsX = int32(C{2});
-groundTruthPosRegionsY = int32(C{3});
-groundTruthPosRegionsW = int32(C{4});
-groundTruthPosRegionsH = int32(C{5});
-
-dImgs = {};
-dRegions = [];
-count = 1;
-
-for i = 1:length(detectedRegionsX)
-	flag = 0;
-	if isempty(dImgs)
-		flag = 1;	
-	else
-		for j = 1:length(dImgs)
-			if strcmp(dImgs{j}, strtrim(detectedImgs(i,:))) ...
-			& dRegions(j, 1) == detectedRegionsX(i) ...
-			& dRegions(j, 2) == detectedRegionsY(i) ...
-			& dRegions(j, 3) == detectedRegionsW(i) ...
-			& dRegions(j, 4) == detectedRegionsH(i)
-				flag = 0;
-			else				
-
-				flag = 1;
-				
-			end
-		end	
-	end
 	
-	if flag == 1
-		dImgs{count} = strtrim(detectedImgs(i,:));
-		dRegions(count, 1) = detectedRegionsX(i);
-		dRegions(count, 2) = detectedRegionsY(i);
-		dRegions(count, 3) = detectedRegionsW(i);
-		dRegions(count, 4) = detectedRegionsH(i);		
-		count = count + 1;				
-	end		
-end
+	outputDirectory = ('data/extendedDataset/output/');
+	gtDirectory = ('data/extendedDataset/output/groundTruth/');
 
 
-truePositive = 0;
-falsePositive = 0;
-for i = 1:length(dImgs)
-	dImg = dImgs{i};
-	dRegion(1, 1) = dRegions(i, 1);
-	dRegion(1, 2) = dRegions(i, 2);
-	dRegion(1, 3) = dRegions(i, 3);
-	dRegion(1, 4) = dRegions(i, 4);
-	
-	detectedArea = detectedRegionsW(i) * detectedRegionsH(i);
-	
-	for j = 1:length(groundTruthPosRegionsX)
-		gtImg = strtrim(groundTruthPosImgs(j,:));
-		gtRegion(1, 1) = groundTruthPosRegionsX(j);
-		gtRegion(1, 2) = groundTruthPosRegionsY(j);
-		gtRegion(1, 3) = groundTruthPosRegionsW(j);
-		gtRegion(1, 4) = groundTruthPosRegionsW(j);
+	fileID = fopen(strcat(outputDirectory, outputFile));
+	C = textscan(fileID, '%s %d %d %d %d %f','delimiter', ',', 'EmptyValue', -Inf);
+	fclose(fileID);
+
+	detectedImgs = char(C{1});
+	detectedRegionsX = int32(C{2});
+	detectedRegionsY = int32(C{3});
+	detectedRegionsW = int32(C{4});
+	detectedRegionsH = int32(C{5});
+	detectedRegionScore = single(C{6});
+
+	fileID = fopen(strcat(gtDirectory, gtFile));
+	C = textscan(fileID, '%s %d %d %d %d','delimiter', ',', 'EmptyValue', -Inf);
+	fclose(fileID);
+
+	groundTruthPosImgs = char(C{1});
+	groundTruthPosRegionsX = int32(C{2});
+	groundTruthPosRegionsY = int32(C{3});
+	groundTruthPosRegionsW = int32(C{4});
+	groundTruthPosRegionsH = int32(C{5});
+
+	truePositive = 0;
+	falsePositive = 0;
+	for i = 1:length(detectedRegionsX)
+		dImg = strtrim(detectedImgs(i,:));
+		dRegion(1, 1) = detectedRegionsX(i);
+		dRegion(1, 2) = detectedRegionsY(i);
+		dRegion(1, 3) = detectedRegionsW(i);
+		dRegion(1, 4) = detectedRegionsH(i);	
 		
-		gtArea = groundTruthPosRegionsW(j) * groundTruthPosRegionsW(j);
+		detectedArea = detectedRegionsW(i) * detectedRegionsH(i);
+		
+		if detectedRegionScore(i) > detectionScore
 
-		if strcmp(dImg, gtImg) 			
-			largerArea = max(detectedArea, gtArea);
-			
-			if rectint(dRegion, gtRegion) > (0.5*detectedArea)
-				truePositive = truePositive + 1;	
-			else
-				falsePositive = falsePositive + 1;
-			end			
+				for j = 1:length(groundTruthPosRegionsX)
+				gtImg = strtrim(groundTruthPosImgs(j,:));
+				gtRegion(1, 1) = groundTruthPosRegionsX(j);
+				gtRegion(1, 2) = groundTruthPosRegionsY(j);
+				gtRegion(1, 3) = groundTruthPosRegionsW(j);
+				gtRegion(1, 4) = groundTruthPosRegionsW(j);
+				
+				gtArea = groundTruthPosRegionsW(j) * groundTruthPosRegionsW(j);
+
+				if strcmp(dImg, gtImg) 			
+					largerArea = max(detectedArea, gtArea);
+					
+					if rectint(dRegion, gtRegion) > (0.5*largerArea)
+						truePositive = truePositive + 1;	
+					else
+						falsePositive = falsePositive + 1;
+					end			
+				end
+			end
+
+
 		end
-	end
-	
-end	
+		
+		
+	end	
 
-falseNegatives = length(groundTruthPosRegionsX) - truePositive;
-precision =  truePositive/(truePositive+falsePositive);%-- Precision = TP/(TP + FP) 
-recall =  truePositive/length(groundTruthPosRegionsX);%-- Recall = TP/nP,
+	falseNegatives = length(groundTruthPosRegionsX) - truePositive;
+	precision =  truePositive/(truePositive+falsePositive);%-- Precision = TP/(TP + FP) 
+	recall =  truePositive/length(groundTruthPosRegionsX);%-- Recall = TP/nP,
 
-recall 
-precision 
-truePositive
-falseNegatives
-falsePositive
+	M ={};
+	M{1, 1} = detectionScore;
+	M{2, 1} = ('Precision');
+	M{2, 2} = precision;
+	M{3, 1} = ('Recall');
+	M{3, 2} = recall;
+	M{4, 1} = ('True Positives');
+	M{4, 2} = truePositive;
+	M{5, 1} = ('False Positives');
+	M{5, 2} = falsePositive;
+	M{6, 1} = ('False Negatives');
+	M{6, 2} = falseNegatives;
 
+	M{7, 1} = ('  ');
+	dlmcell('P-R.csv', M, ',', '-a');
+
+	%recall 
+	%precision 
+	%truePositive
+	%falseNegatives
+	%falsePositive
+
+end
 
